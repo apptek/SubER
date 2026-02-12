94d3a1679b89faa043255cfa93d217f59d100331
Patrick Wilken <pwilken@apptek.com>
2026-02-12 20:29:29 +0000

    Add more tests for zh, ja, ko

--- tests/test_length_ratio.py
+++ tests/test_length_ratio.py
@@ -37,41 +37,6 @@ class LengthRatioTest(unittest.TestCase):
 
         self.assertAlmostEqual(length_ratio, 7 / 9 * 100, places=3)
 
-    def test_length_ratio_japanese(self):
-        # TODO: Not sure what to expect here, some numbers are split into characters, others not. (Without commas it
-        # looks even less consistent to me.) Need language expertise. :D
-        # Could also test kanji, but then it would be the same as Chinese, i.e. characters?
-        reference_file_content = """
-            1
-            00:00:00,000 --> 00:00:01,000
-            いち、に、さん。
-
-            2
-            00:00:01,000 --> 00:00:02,000
-            ご、ろく
-            しち、はち？"""
-
-        hypothesis_file_content = """
-            1
-            00:00:00,000 --> 00:00:01,000
-            いち、に。
-
-            2
-            00:00:01,000 --> 00:00:02,000
-            し、ご
-
-            3
-            00:00:01,500 --> 00:00:02,000
-            ろく？"""
-
-        reference_subtitles = create_temporary_file_and_read_it(reference_file_content)
-        hypothesis_subtitles = create_temporary_file_and_read_it(hypothesis_file_content)
-
-        length_ratio = calculate_length_ratio(
-            hypothesis=hypothesis_subtitles, reference=reference_subtitles, language="ja")
-
-        self.assertAlmostEqual(length_ratio, 10 / 16 * 100, places=3)
-
     def test_length_ratio_chinese(self):
         # Should be split into characters, including punctuation, except for the English words, which are handled
         # separately by the tokenizer.
@@ -107,6 +72,41 @@ class LengthRatioTest(unittest.TestCase):
 
         # As in English test_length_ratio(), but both reference and hypothesis "plus three" tokens.
         self.assertAlmostEqual(length_ratio, 10 / 12 * 100, places=3)
+
+    def test_length_ratio_japanese(self):
+        # TODO: Not sure what to expect here, some numbers are split into characters, others not. (Without commas it
+        # looks even less consistent to me.) Need language expertise. :D
+        # Could also test kanji, but then it would be the same as Chinese, i.e. characters?
+        reference_file_content = """
+            1
+            00:00:00,000 --> 00:00:01,000
+            いち、に、さん。
+
+            2
+            00:00:01,000 --> 00:00:02,000
+            ご、ろく
+            しち、はち？"""
+
+        hypothesis_file_content = """
+            1
+            00:00:00,000 --> 00:00:01,000
+            いち、に。
+
+            2
+            00:00:01,000 --> 00:00:02,000
+            し、ご
+
+            3
+            00:00:01,500 --> 00:00:02,000
+            ろく？"""
+
+        reference_subtitles = create_temporary_file_and_read_it(reference_file_content)
+        hypothesis_subtitles = create_temporary_file_and_read_it(hypothesis_file_content)
+
+        length_ratio = calculate_length_ratio(
+            hypothesis=hypothesis_subtitles, reference=reference_subtitles, language="ja")
+
+        self.assertAlmostEqual(length_ratio, 10 / 16 * 100, places=3)
 
     def test_length_ratio_korean(self):
         # Tokenizer expected to split into numbers.
